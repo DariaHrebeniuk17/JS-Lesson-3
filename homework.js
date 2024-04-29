04a3f01d6db0ea377af93221e132a2bfa53bf540
Daria Hrebeniuk <144627994+DariaHrebeniuk17@users.noreply.github.com>
2024-04-29 18:51:06 +0300

    Add files via upload

---
+++ homework.js
@@ -0,0 +1,64 @@
+// У вас є масив об’єктів fruts, і в кожному з них є name
+// Напишіть код, який перетворює їх в масив імен.
+
+/*const fruits = [
+    {id: 0, name: "Apple"},
+    {id: 1, name: "Tomat"},
+    {id: 3, name: "Cherry"},
+    {id: 4, name: "Orrange"},
+];
+
+const fruitNames = fruits.map(fruit => fruit.name);
+
+console.log(fruitNames);*/
+
+
+
+
+
+// for (var i = 2; i <= 10; i += 2) {
+//     console.log(i);
+// };
+
+
+
+
+//  -- 3 --
+//Замініть цикл "for" на "while"
+// for (let i = 0; i < 5; i++) {
+//     console.lpg( `цифра ${i}!` );
+//   }
+
+// let i = 0;
+
+// while (1 < 5) {
+//     console.log(`figure ${i}!`);
+// };
+
+
+// let number;
+
+// do {
+//     number = prompt("Будь ласка введіть число більше за 100:", "");
+// } while (number !== nul && (isNaN(number) || Number(number) <= 100));
+
+// if (number !== null) {
+//     console.log(`Ви ввели число більше за 100:" ${number}`);
+// } else {
+//     console.log("Ви скасували ввід аюо ввели порожній рядок.");
+// };
+
+
+
+// const girls = [
+//     { age: 23, name: "Оля"},
+//     { age: 29, name: "Аня"},
+//     { age: 10, name: "Юля"},
+//     { age: 20, name: "Катя"},
+// ];
+
+// const totalAge = girls.reduce((acc, girl) => acc + girl.age, 0);
+
+// const averageAge = totalAge / girls.length;
+
+// console.log("Середній вік", averageAge);
